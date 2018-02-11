a720bcddc81465e2507d3e51cc387dbcbdc22114
Lucy W. Owen <lowen@ndoli.hpcc.dartmouth.edu>
2018-02-11 14:32:06 -0500

    model named wrong.  fixed

--- full_mats/full_mats_job_submit.py
+++ full_mats/full_mats_job_submit.py
@@ -19,9 +19,9 @@ except:
 # each job command should be formatted as a string
 job_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'full_mats.py')
 files = glob.glob(os.path.join(config['datadir'],'*.bo'))
-# options for model: 'pyFR_locs', 'example_model', 'gray_mask_6mm_brain'
+# options for model: 'pyFR_union', 'example_model', 'gray_mask_6mm_brain'
 # model = str('gray_mask_6mm_brain')
-model = str('pyFR_locs')
+model = str('pyFR_union')
 job_commands = map(lambda x: x[0]+" "+str(x[1])+" " + model, zip([job_script]*len(files), files))
 
 # job_names should specify the file name of each script (as a list, of the same length as job_commands)
