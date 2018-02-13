6e90af8e476afd7e4fb8ed7f6d02e4bfe3806560
lucy <lucywowen@gmail.com>
2018-02-13 14:03:42 -0500

    reformatted create_gif to check if nii exists first. also created dictionary of gif arguements to make changing parameters easier

--- gif/create_gif.py
+++ gif/create_gif.py
@@ -4,6 +4,7 @@ import sys
 import os
 import copy
 import pandas as pd
+import nibabel as nib
 from config import config
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
@@ -14,6 +15,17 @@ import seaborn as sns; sns.set()
 
 cmap=plt.cm.get_cmap('Spectral')
 
+gif_args = {'window_min': 1000,
+            'window_max': 1101,
+            'cmap': cmap,
+            'display_mode': 'lyrz',
+            'threshold': 0,
+            'plot_abs': False,
+            'colorbar': False,
+            'vmin': -5,
+            'vmax': 5}
+
+
 model_template = sys.argv[1]
 
 model_dir = os.path.join(config['datadir'], model_template)
@@ -21,6 +33,7 @@ model_dir = os.path.join(config['datadir'], model_template)
 results_recon_dir = os.path.join(config['resultsdir'], model_template + '_recon')
 
 results_obs_dir = os.path.join(config['resultsdir'], model_template + '_obs')
+
 
 try:
     os.stat(results_recon_dir)
@@ -32,40 +45,45 @@ try:
 except:
     os.makedirs(results_obs_dir)
 
-model_data = os.path.join(model_dir, model_template + '.mo')
+recon_file = os.path.join(results_recon_dir, 'recon.nii')
+obs_file = os.path.join(results_obs_dir, 'obs.nii')
 
+if not os.path.exists(recon_file):
 
-model = se.load(intern(model_data))
+    model_data = os.path.join(model_dir, model_template + '.mo')
 
-bo = se.load('example_data')
-bo.info()
+    model = se.load(intern(model_data))
 
-bor = model.predict(bo)
+    bo = se.load('example_data')
+    bo.info()
 
-zbor = copy.copy(bor)
-zbor.data = pd.DataFrame(bor.get_zscore_data())
+    bor = model.predict(bo)
 
-# find the observed indices
-obs_inds = [i for i, x in enumerate(bor.label) if x == 'observed']
+    zbor = copy.copy(bor)
+    zbor.data = pd.DataFrame(bor.get_zscore_data())
 
-# make a copy of the brain object
-o_bo = copy.copy(zbor)
+    # find the observed indices
+    obs_inds = [i for i, x in enumerate(bor.label) if x == 'observed']
 
-# replace fields with indexed data and locations
-o_bo.data = pd.DataFrame(o_bo.get_data()[:, obs_inds])
-o_bo.locs = pd.DataFrame(o_bo.get_locs()[obs_inds], columns=['x', 'y', 'z'])
+    # make a copy of the brain object
+    o_bo = copy.copy(zbor)
 
+    # replace fields with indexed data and locations
+    o_bo.data = pd.DataFrame(o_bo.get_data()[:, obs_inds])
+    o_bo.locs = pd.DataFrame(o_bo.get_locs()[obs_inds], columns=['x', 'y', 'z'])
 
-if model_template == 'gray_mask_6mm_brain':
-    nii_recon = zbor.to_nii(template='6mm')
-    nii_obs = o_bo.to_nii(template='6mm')
+    if model_template == 'gray_mask_6mm_brain':
+        nii_recon = zbor.to_nii(filepath=recon_file, template='6mm')
+        nii_obs = o_bo.to_nii(filepath=obs_file, template='6mm')
+    else:
+        nii_recon = zbor.to_nii(filepath=recon_file, template='20mm')
+        nii_obs = o_bo.to_nii(filepath=obs_file, template='20mm')
+
 else:
-    nii_recon = zbor.to_nii(template='20mm')
-    nii_obs = o_bo.to_nii(template='20mm')
+    nii_recon = nib.load(recon_file)
 
-make_gif_pngs(nii_recon, gif_path=results_recon_dir, window_min=1000, window_max=1256, cmap=cmap, display_mode='lyrz', threshold=0,
-              plot_abs=False, colorbar=False, vmin=-3, vmax=3)
+    nii_obs = nib.load(obs_file)
 
+make_gif_pngs(nii_recon, gif_path=results_recon_dir, **gif_args)
 
-make_gif_pngs(nii_obs, gif_path=results_obs_dir, window_min=1000, window_max=1256, cmap=cmap, display_mode='lyrz', threshold=0,
-              plot_abs=False, colorbar=False, vmin=-3, vmax=3)
+make_gif_pngs(nii_obs, gif_path=results_obs_dir, **gif_args)
