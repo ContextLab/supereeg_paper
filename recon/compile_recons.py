9d95198c349cdf4557eb1afd83d5e24a15231d58
lucy <lucywowen@gmail.com>
2018-06-06 14:38:05 -0400

    redo compile code

--- recon/compile_recons.py
+++ recon/compile_recons.py
@@ -13,27 +13,23 @@ radius = sys.argv[2]
 
 results_dir = os.path.join(config['resultsdir'], model_template+ '_' + radius)
 
-across_dir = os.path.join(results_dir, 'across_subjects')
-within_dir = os.path.join(results_dir, 'within_subjects')
-all_dir = os.path.join(results_dir, 'all_subjects')
+dir = os.path.join(results_dir, sys.argv[3])
 
-dirs = [across_dir, within_dir, all_dir]
 
-for d in dirs:
+files = glob.glob(os.path.join(dir, '*.npz'))
 
-    files = glob.glob(os.path.join(d, '*.npz'))
+all_corrs = pd.DataFrame()
 
-    all_corrs = pd.DataFrame()
+for i in files:
 
-    for i in files:
+    compile_temp = compile_corrs(config['datadir'], i)
+    if all_corrs.empty:
+        all_corrs = compile_temp
+    else:
+        all_corrs = all_corrs.append(compile_temp)
+        all_corrs.to_csv(os.path.join(dir, 'all_corrs.csv'))
 
-        compile_temp = compile_corrs(config['datadir'], i)
-        if all_corrs.empty:
-            all_corrs = compile_temp
-        else:
-            all_corrs = all_corrs.append(compile_temp)
+all_corrs['Density'] = density(all_corrs['R'].tolist(), 3)
 
-    all_corrs['Density'] = density(all_corrs['R'].tolist(), 3)
-
-    print(os.path.basename(d) + ': ' + all_corrs['Correlation'].mean())
-    all_corrs.to_csv(os.path.join(d, 'all_corrs.csv'))
+print(os.path.basename(dir) + ': ' + all_corrs['Correlation'].mean())
+all_corrs.to_csv(os.path.join(dir, 'all_corrs.csv'))
