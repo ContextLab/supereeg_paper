823f2fa0b0471a241e9c09123f53746647bfd0c5
lucy <lucywowen@gmail.com>
2018-08-22 21:17:31 -0400

    fixed path to nifti data

--- simulation/convert_fmri_bo.py
+++ simulation/convert_fmri_bo.py
@@ -17,14 +17,14 @@ fmri_dir = config['fmri_datadir']
 niis =glob.glob(os.path.join(config['fmri_datadir'], '*.nii'))
 
 
-#for i in list(range(1, len(niis)+1)):
-for i in list(range(1, 2)):
+for i in list(range(1, len(os.listdir(config['fmri_datadir']))+1)):
 
-    bo_file = os.path.join(results_dir+'sub-%d-task-intact1' % i)
+
+    bo_file = os.path.join(results_dir, 'sub-%d' % i, 'func', 'sub-%d-task-intact1' % i)
 
     if not os.path.exists(bo_file):
         bo = se.Brain(se.load(os.path.join(fmri_dir,'sub-%d-task-intact1.nii' % i)))
-        bo.save(os.path.join(results_dir+'sub-%d-task-intact1' % i))
+        bo.save(os.path.join(results_dir, 'sub-%d-task-intact1' % i))
 
 
 print('done converting brain objects')
