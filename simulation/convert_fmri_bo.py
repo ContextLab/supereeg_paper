a071cab9545d07e543774813e477c300f8f15e44
lucy <lucywowen@gmail.com>
2018-08-23 13:54:24 -0400

    converts intact1 and intact2 pieman data to bos

--- simulation/convert_fmri_bo.py
+++ simulation/convert_fmri_bo.py
@@ -1,7 +1,6 @@
 
 import supereeg as se
 import os
-import glob as glob
 from config import config
 
 results_dir = config['bof_datadir']
@@ -17,13 +16,24 @@ fmri_dir = config['fmri_datadir']
 for i in list(range(1, len(os.listdir(config['fmri_datadir']))+1)):
 
 
-    bo_file = os.path.join(results_dir, 'sub-%d-task-intact1' % i)
+    bo_file = os.path.join(results_dir, 'sub-%d-task-intact1' % i + '.bo')
 
     if not os.path.exists(bo_file):
 
         try:
             ## need to do this for intact1 and intact 2!
-            bo = se.Brain(se.load(os.path.join(fmri_dir,'sub-%d' % i, 'func', 'sub-%d-task-intact1' % i + '.nii')))
+            bo = se.Brain(se.load(os.path.join(fmri_dir, 'sub-%d-task-intact1' % i + '.nii')))
+            bo.save(bo_file)
+        except:
+            print(bo_file + '_issue')
+
+    bo_file = os.path.join(results_dir, 'sub-%d-task-intact2' % i)
+
+    if not os.path.exists(bo_file):
+
+        try:
+            ## need to do this for intact1 and intact 2!
+            bo = se.Brain(se.load(os.path.join(fmri_dir, 'sub-%d-task-intact2' % i + '.nii')))
             bo.save(bo_file)
         except:
             print(bo_file + '_issue')
